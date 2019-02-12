d6330dacfe9972a893ab6d51db839847615eb6f8
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2019-02-12 20:56:41 +0100

    Implement basic simulation of Mercury

--- anim-mercury.py
+++ anim-mercury.py
@@ -3,11 +3,13 @@ from itertools import cycle, chain
 import subprocess
 from pathlib import Path
 import shutil
+import time
 
 import numpy as np
 
 from geometry import make_grid, flamm_projection
 from util import neighbours
+import sim
 
 # image dimensions
 WIDTH = 600
@@ -19,7 +21,7 @@ BACKGROUND = "#2c2c2c"
 
 # grid-lines
 HLINES, VLINES = make_grid((-200, -200), (WIDTH+200, HEIGHT+200),
-                           nlines=(10, 10), resolution=(50, 50))
+                           nlines=(20, 20), resolution=(50, 50))
 
 def init_directory(path, overwrite):
     if path.exists():
@@ -90,30 +92,65 @@ def setup_window(width, height, background):
     canvas.pack()
     return window, canvas
 
-def draw_line(canvas, line, centre, rs, colour):
-    for p0, p1 in neighbours(flamm_projection(line, centre, rs, np.array((0, 0)))):
+def draw_line(canvas, line, colour):
+    for p0, p1 in neighbours(line):
         if not np.ma.is_masked(p0) and not np.ma.is_masked(p1):
             canvas.create_line(*p0[:2], *p1[:2], fill=colour)
 
 def draw_grid(canvas, centre, rs, colour):
     for line in chain(HLINES, VLINES):
-        draw_line(canvas, line, centre, rs, colour)
+        draw_line(canvas, flamm_projection(line, centre, rs, np.array((0, 0))), colour)
+
+def sim2screen(point, sim_bounds, screen_size):
+    return point*screen_size/2/sim_bounds + screen_size/2
 
 def main():
     # frames = FrameManager(Path(__file__).resolve().parent/"frames", True)
 
     window, canvas = setup_window(WIDTH, HEIGHT, BACKGROUND)
 
-    rss = np.linspace(0, 100, 100)
-    for i, rs in enumerate(cycle(chain(rss, reversed(rss)))):
+    mercury = sim.CBody.mercury()
+    sun = sim.CBody.sun()
 
-        canvas.delete("all")
-        canvas.create_rectangle(0, 0, WIDTH, HEIGHT, fill=BACKGROUND)
+    dt = 2.0 * np.linalg.norm(mercury.v) / mercury.acc / 2 # Time step
+    alpha = 5e6 # Strength of 1/r**3 term
+    beta = 0.0 # Strength of 1/r**4 term
 
-        canvas.create_oval(CENTRE[0]-rs, CENTRE[1]-rs, CENTRE[0]+rs, CENTRE[1]+rs, fill="black")
-        draw_grid(canvas, CENTRE, rs, "gray")
+    sim_bounds = np.array((5, 5))
+    screen_size = np.array((WIDTH, HEIGHT))
+
+    mercury_oval = None
+
+    sx = sim2screen(sun.x, sim_bounds, screen_size)
+
+    canvas.create_rectangle(0, 0, WIDTH, HEIGHT, fill=BACKGROUND)
+    draw_grid(canvas, CENTRE, 20, "#404040")
+    canvas.create_oval(sx[0]-20, sx[1]-20,
+                       sx[0]+20, sx[1]+20,
+                       fill="yellow")
+    
+    trajectory = [sim2screen(mercury.x, sim_bounds, screen_size)]
+    while True:
+        mercury = sim.advance(mercury, dt, alpha, beta)
+        trajectory.append(sim2screen(mercury.x, sim_bounds, screen_size))
+
+        # canvas.delete("all")
+        if mercury_oval is not None:
+            canvas.delete(mercury_oval)
+
+
+        mx = sim2screen(mercury.x, sim_bounds, screen_size)
+
+        draw_line(canvas, trajectory[-2:], "gray")
+
+        mercury_oval = canvas.create_oval(mx[0]-10, mx[1]-10,
+                                          mx[0]+10, mx[1]+10,
+                                          fill="red")
+
         window.update()
 
+        time.sleep(1/60)
+        
         # frames.save_frame(canvas, png=True, resolution=150)
         # if i == 50:
             # break
@@ -123,6 +160,32 @@ def main():
     # print("Created animation mercury.gif")
 
     mainloop()
+
+# animate grid    
+# def main():
+#     # frames = FrameManager(Path(__file__).resolve().parent/"frames", True)
+
+#     window, canvas = setup_window(WIDTH, HEIGHT, BACKGROUND)
+
+#     rss = np.linspace(0, 100, 100)
+#     for i, rs in enumerate(cycle(chain(rss, reversed(rss)))):
+
+#         canvas.delete("all")
+#         canvas.create_rectangle(0, 0, WIDTH, HEIGHT, fill=BACKGROUND)
+
+#         canvas.create_oval(CENTRE[0]-rs, CENTRE[1]-rs, CENTRE[0]+rs, CENTRE[1]+rs, fill="black")
+#         draw_grid(canvas, CENTRE, rs, "gray")
+#         window.update()
+
+#         # frames.save_frame(canvas, png=True, resolution=150)
+#         # if i == 50:
+#             # break
+
+#     # print("Creating GIF")
+#     # frames.convert_to_gif("mercury.gif")
+#     # print("Created animation mercury.gif")
+
+#     mainloop()
 
 
 if __name__ == "__main__":
