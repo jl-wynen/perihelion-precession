9067dbcb5c61fc7cc70ba27c502ef82308787cc9
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2019-02-13 16:12:16 +0100

    Add extra function to animate orbit

--- anim-mercury.py
+++ anim-mercury.py
@@ -10,6 +10,9 @@ import sim
 WIDTH = 600
 HEIGHT = 600
 
+# resolution of output bitmaps
+OUTPUT_RESOLUTION = 150
+
 # grid-lines
 HLINES, VLINES = sim.make_grid((-10, -10), (10, 10),
                                nlines=(20, 20), resolution=(50, 50))
@@ -25,6 +28,30 @@ def draw_grid(anim, lines, centre, rs, colour):
         #     q1 = sim.radial_transform(p1, centre, rs, 1, 4)
         #     draw_line(canvas, (q0, q1), colour, "grid")
 
+def anim_orbit(anim, mercury, nframes, dt, alpha, beta, frames=None):
+    mercury_oval = None
+    trajectory = [mercury.x]
+
+    for _ in range(nframes):
+        start = time.time()
+
+        mercury = sim.advance(mercury, dt, alpha, beta)
+        trajectory.append(mercury.x)
+
+        anim.clear(mercury_oval)
+        anim.line(trajectory[-2:], "#606060", lw=2, tags="trajectory")
+        mercury_oval = anim.circle(mercury.x, 0.1, fill="red", tags="mercury")
+        anim.update()
+
+        if frames:
+            frames.save_frame(anim.canvas, ps=True, png=True, resolution=OUTPUT_RESOLUTION)
+
+        end = time.time()
+        time_diff = end-start
+        time.sleep(max(1/60 - time_diff, 0))
+
+    return mercury
+
 def main():
     anim = sim.tk.Tk(sim.Transform((-5, -5), (5, 5), (0, 0), (0+WIDTH, 0+HEIGHT)),
                      background="#161616")
@@ -38,28 +65,11 @@ def main():
     alpha = 5e6 # Strength of 1/r**3 term
     beta = 0.0 # Strength of 1/r**4 term
 
-    mercury_oval = None
-
     anim.draw_background()
     draw_grid(anim, chain(HLINES, VLINES), np.array((0, 0)), 0.01, "#404040")
     anim.circle(sun.x, 0.5, fill="yellow")
 
-    trajectory = [mercury.x]
-    while True:
-    # for i in range(50):
-        mercury = sim.advance(mercury, dt, alpha, beta)
-        trajectory.append(mercury.x)
-
-        anim.clear(mercury_oval)
-
-        anim.line(trajectory[-2:], "#606060", lw=2)
-        mercury_oval = anim.circle(mercury.x, 0.1, fill="red", tags="mercury")
-
-        anim.update()
-
-        time.sleep(1/60)
-
-    #     frames.save_frame(canvas, png=True, resolution=150)
+    mercury = anim_orbit(anim, mercury, 100000, dt, alpha, beta)
 
     # print("Creating GIF")
     # frames.convert_to_gif("mercury.gif")
