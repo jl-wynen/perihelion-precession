aefd9b34733ec7758d24fb86cb7eeb71ad1559c9
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2019-03-11 19:02:05 +0100

    Update animation script

--- anim-mercury.py
+++ anim-mercury.py
@@ -1,4 +1,4 @@
-from itertools import cycle, chain
+from itertools import chain
 import time
 from pathlib import Path
 
@@ -13,8 +13,8 @@ WORLD_WIDTH = 16
 WORLD_HEIGHT = 16
 
 # size of output pixel images
-OUTPUT_WIDTH = 512
-OUTPUT_HEIGHT = 512
+OUTPUT_WIDTH = 1024
+OUTPUT_HEIGHT = 1024
 OUTPUT_SIZE = (OUTPUT_WIDTH, OUTPUT_HEIGHT)
 
 # grid-lines
@@ -57,11 +57,11 @@ def anim_grid(anim, rsiter, frames=None):
         time.sleep(max(1/60 - time_diff, 0))
 
 
-def anim_orbit(anim, mercury, nframes, integrator_params, tracker=None, frames=None):
+def anim_orbit(anim, mercury, iterator, integrator_params, tracker=None, frames=None):
     mercury_oval = None
     trajectory = [mercury.x]
 
-    for _ in range(nframes):
+    for _ in iterator:
         start = time.time()
 
         mercury = sim.advance(mercury, tracker=tracker, **integrator_params)
@@ -69,7 +69,7 @@ def anim_orbit(anim, mercury, nframes, integrator_params, tracker=None, frames=N
 
         anim.clear(mercury_oval)
         anim.line(trajectory[-2:], TRAJECTORY_COLOUR, lw=2, tags="trajectory")
-        mercury_oval = anim.circle(mercury.x, 0.1, fill=MERCURY_COLOUR, tags="mercury")
+        mercury_oval = anim.circle(mercury.x, 0.2, fill=MERCURY_COLOUR, tags="mercury")
         anim.update()
 
         if frames:
@@ -80,6 +80,52 @@ def anim_orbit(anim, mercury, nframes, integrator_params, tracker=None, frames=N
         time.sleep(max(1/60 - time_diff, 0))
 
     return mercury
+
+def sleep_animation(anim, duration, frames=None):
+    nframes = int(duration*60)
+    for _ in range(nframes):
+        start = time.time()
+        anim.update()
+
+        if frames:
+            frames.save_frame(anim.canvas, ps=True, png=True, size=OUTPUT_SIZE)
+
+        end = time.time()
+        time_diff = end-start
+        time.sleep(max(1/60 - time_diff, 0))
+
+def until_perihelion(niterations, anim, reference_point, colour):
+    """
+    Construct a tracker and an animation iterator to animate until
+    niterations perihelions have been found.
+    The tracker marks all except for the first perihelion btu cheats and places all
+    pings at the same position. This is to hide numerical inaccuracies in
+    finding the perihelions.
+    """
+
+    nvisited = 0
+    pinger = sim.ExtremaTracker.tk_ping(anim, colour)
+    skip_perihelion = True  # skip the next perihelion that is found?
+    perihelion = None  # previously found perihelion, use this as THE true location
+
+    def _on_perihelion(point):
+        nonlocal nvisited, skip_perihelion, perihelion
+        nvisited += 1
+        if skip_perihelion:
+            skip_perihelion = False
+        elif perihelion is None:
+            perihelion = point
+            pinger(perihelion)
+        else:
+            pinger(perihelion)
+
+    def _iterator():
+        count = 0
+        while nvisited < niterations:
+            yield count
+
+    tracker = sim.ExtremaTracker(reference_point, _on_perihelion)
+    return tracker, _iterator()
 
 def write_animation(frames, gif=False, mp4=True):
     if gif:
@@ -105,21 +151,31 @@ def main():
     mercury = sim.CBody.mercury()
     sun = sim.CBody.sun()
 
-    integrator_params = {"length": 2.0 * np.linalg.norm(mercury.v) / mercury.acc / 2,
-                         "nsteps": 20,
+    integrator_params = {"length": 2.0 * np.linalg.norm(mercury.v) / mercury.acc / 2 *2,
+                         "nsteps": 20*2,
                          "alpha": 0.0,
                          "beta": 0.0}
 
     anim.draw_background()
-    draw_grid(anim, chain(HLINES, VLINES), np.array((0, 0)), 0.05, GRID_COLOUR)
-    anim.circle(sun.x, 0.5, fill=SUN_COLOUR, tags="sun")
-    tracker = sim.ExtremaTracker(sun.x, sim.ExtremaTracker.tk_ping(anim, PERIHELION_COLOUR))
+    draw_grid(anim, chain(HLINES, VLINES), np.array((0, 0)), 0, GRID_COLOUR)
+    anim.circle(sun.x, 0.8, fill=SUN_COLOUR, tags="sun")
 
-    # mercury = anim_orbit(anim, mercury, 4000, integrator_params, tracker=tracker)
-    # anim_grid(anim, np.linspace(0, 0.05, 10))
-    # anim.clear("mercury")
-    integrator_params["alpha"] = 5e6
-    mercury = anim_orbit(anim, mercury, 400, integrator_params, tracker=tracker, frames=frames)
+    # newtonian
+    tracker, iterator = until_perihelion(4, anim, sun.x, PERIHELION_COLOUR)
+    mercury = anim_orbit(anim, mercury, iterator, integrator_params,
+                         tracker=tracker, frames=frames)
+
+    # transform grid
+    anim.clear("mercury")
+    sleep_animation(anim, 0.5, frames)
+    anim_grid(anim, np.linspace(0, 0.017, 60), frames=frames)
+    sleep_animation(anim, 0.5, frames)
+
+    # switch on GR
+    integrator_params["beta"] = 2e6
+    tracker = sim.ExtremaTracker(sun.x, sim.ExtremaTracker.tk_ping(anim, PERIHELION_COLOUR))
+    mercury = anim_orbit(anim, mercury, range(600), integrator_params,
+                         tracker=tracker, frames=frames)
 
     write_animation(frames)
 
@@ -128,5 +184,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# ffmpeg -framerate 25 -i %04d.png -y -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p anim.mp4
